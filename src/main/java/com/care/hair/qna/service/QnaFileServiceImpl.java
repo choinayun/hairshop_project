5b74eab66354c07873695832563efea2fd83c39d
duhwan1001 <kdhz2012@gmail.com>
2022-10-04 21:16:17 +0900

    프로젝트 최종파일

--- src/main/java/com/care/hair/qna/service/QnaFileServiceImpl.java
+++ src/main/java/com/care/hair/qna/service/QnaFileServiceImpl.java
@@ -24,7 +24,7 @@ public class QnaFileServiceImpl implements QnaFileService {
 	    String sysFileName = 
 	    		simpl.format(calendar.getTime()) + file.getOriginalFilename();
 	    File saveFile = new File(IMAGE_REPO+"/"+sysFileName);
-	    System.out.println("sysFileName : "+sysFileName);
+	    
 	    try {
 	    	file.transferTo(saveFile);//해당 위치에 파일 저장
 	    }catch (Exception e) {
